e7db28c603ba96a2ed30bc8e3a0060d35dbaf6fb
Alasdair Sykes <alasdair.sykes@gmail.com>
2020-06-17 13:24:26 +0100

    Finished shelter belt functions and implemented.

--- scenario-functions/shelter-belt-functions.R
+++ scenario-functions/shelter-belt-functions.R
@@ -1,22 +1,17 @@
 
 library(tidyverse)
 
-# temp <- Dat_dummy
-# Dat_dummy <- temp
+# new environment
+sb_env <- new.env()
 
-add_tree_data <- function(df, spp_short, felling_age, sys_type){
+sb_env$add_tree_data <- function(df, spp_short, felling_age){
   
   # read in tree data
   Dat_SB <- read_rds("shelter-belt-data-preprocessing/shelter-belt-data-clean.rds")
   
-  # filter spp and tree density type
-  if(sys_type == "livestock") is_tight <- TRUE
-  if(sys_type == "crop") is_tight <- FALSE
-  
+  # filter spp
   Dat_SB <- Dat_SB %>%
-    filter(spp == spp_short,
-           tight == is_tight) %>%
-    select(-tight)
+    filter(spp == spp_short)
   
   # filter felling age
   felling_age <- round(felling_age / 5, 0) * 5
@@ -24,35 +19,29 @@ add_tree_data <- function(df, spp_short, felling_age, sys_type){
   Dat_SB <- Dat_SB %>%
     filter(period_end == felling_age)
   
-  # pull vars
-  av_tnpp <- Dat_SB$av_tnpp
-  co2_tha <- Dat_SB$C_cum_total
-  vol_m3ha <- Dat_SB$vol_m3ha
-  spacing <- Dat_SB$spacing
-  type <- Dat_SB$type
-  
-  # tnpp ratio
-  tnpp_ratio <- df$tree_tnpp / av_tnpp
+  # build tree data
+  Dat_treedat <- read_rds("simulation-base-data/crop-base-data.rds") %>%
+    distinct(crop) %>%
+    mutate(tight = c(T, T, rep(F, 8))) %>% # tight for livestock, loose for arable
+    left_join(Dat_SB, by = "tight") %>%
+    select(crop, av_tnpp, co2_tha = C_cum_total, vol_m3ha, spacing_m = spacing, wood_type = type)
   
   # add vars to df
   df <- df %>%
-    mutate(treeco2_tha = co2_tha * tnpp_ratio,
-           treevol_m3ha = vol_m3ha * tnpp_ratio,
-           spacing_m = spacing,
-           wood_type = type)
+    left_join(Dat_treedat, by = "crop") %>%
+    mutate(tnpp_ratio = tree_tnpp / av_tnpp,
+           treeco2_tha = co2_tha * tnpp_ratio,
+           treevol_m3ha = vol_m3ha * tnpp_ratio) %>%
+    select(-tnpp_ratio)
   
   return(df)
   
 }
 
-Dat_dummy <- Dat_dummy %>%
-  add_tree_data(spp_short = "SAB", felling_age = 60, sys_type = "livestock")
-
-
 #####################################
 # agroforestry system scaling
 #####################################
-scale_system <- function(df, belt_spacing = 250, width = 10, length = 180){
+sb_env$scale_system <- function(df, belt_spacing, width, length){
   
   # number of complete belts per ha
   nbelts_ha <- (10^4 / length) / (belt_spacing + width)
@@ -70,28 +59,26 @@ scale_system <- function(df, belt_spacing = 250, width = 10, length = 180){
   return(df)
 }
 
-Dat_dummy <- Dat_dummy %>%
-  scale_system()
-
 #####################################
 # crop yield and margin impacts
 #####################################
-
-add_crop_impacts <- function(df){
+sb_env$add_crop_impacts <- function(df){
   
   # yield improvements from Caborn (1957)
   Dat_yieldimp <- read_rds("simulation-base-data/crop-base-data.rds") %>%
     distinct(crop) %>%
-    mutate(frac_yi = c(mean(c(1.34, 1.241)),
-                       mean(c(1.34, 1.241)),
-                       mean(c(1.171, 1.117)),
-                       mean(c(1.171, 1.117)),
-                       mean(c(1.171, 1.117)),
-                       1.169,
-                       mean(c(1.171, 1.117)),
-                       mean(c(1.171, 1.117)),
-                       mean(c(1.232, 1.119)),
-                       mean(c(1.171, 1.117))))
+    # removed old forestry commission data -- so old as to be potentially misleading for newer crop varietals
+    #mutate(frac_yi = c(mean(c(1.34, 1.241)),
+    #                   mean(c(1.34, 1.241)),
+    #                   mean(c(1.171, 1.117)),
+    #                   mean(c(1.171, 1.117)),
+    #                   mean(c(1.171, 1.117)),
+    #                   1.169,
+    #                   mean(c(1.171, 1.117)),
+    #                   mean(c(1.171, 1.117)),
+    #                   mean(c(1.232, 1.119)),
+    #                   mean(c(1.171, 1.117))))
+    mutate(frac_yi = 1.1) # new data from FC x SAC report
   
   df <- df %>%
     left_join(Dat_yieldimp, by = "crop") %>%
@@ -101,138 +88,60 @@ add_crop_impacts <- function(df){
   return(df)
 }
 
-Dat_dummy <- Dat_dummy %>%
-  add_crop_impacts()
-
 #####################################
 # agroforestry cost and revenue
 #####################################
-add_agf_margins <- function(df, felling_age, length, width, discount_rate){
+sb_env$add_agf_margins <- function(df, felling_age, length, width, discount_rate){
   
-  # timber cost datasets
-  # Below is additional Monte Carlo for cost, using data from Burgess et al. (2003). Data assigned into vectors
-  # below are reported values from Burgess' cost analysis (pp. 33-34)
-  
-  MC_n <- 10^4
-  
-  # establishment costs in gbp
-  set_cost <- c(0.95, 1, 0.8, 0.6, 0.25) # purchase cost per set (young tree)
-  prot_cost <- c(0.22, 0.18, 0.24, 0.16) # protection cost per set
-  contmulch_cost <- c(0.1, 0.4) # continuous mulch application per m2 (tree area only)
-  indimulch_cost <- c(0.4, 0.47) # invividual mulch per tree
-  
-  # establishment cost Monte Carlo
-  Dat_cost <- tibble(est_cost_tree =
-                       rpert(n = MC_n, mode = mean(set_cost), min = min(set_cost), max = max(set_cost)) +
-                       rpert(n = MC_n, mode = mean(prot_cost), min = min(prot_cost), max = max(prot_cost)) +
-                       runif(n = MC_n, min = min(indimulch_cost), max = max(indimulch_cost)),
-                     est_cost_m2 = 
-                       runif(n = MC_n, min = min(contmulch_cost), max = max(contmulch_cost))
-  )
-  
-  # establishment labour in hours. Total pruning labour is included here since it is not an annual cost.
-  set_lab <- c(1.5, 1.7, 2.1, 3) / 60 # planting labour per tree
-  prot_lab <- c(0.4, 0.7) / 60 # protection labour per tree
-  contmulchon_lab <- 1.7 / 60 # continuous mulch application labour per m2 (tree area)
-  contmulchoff_lab <- 1.5 / 60 # continuous mulch removal labour per m2 (tree area) (year 8)
-  indimulch_lab <- c(1.7, 2) / 60 # individual mulch per tree
-  
-  # establishment labour Monte Carlo
-  Dat_cost <- Dat_cost %>%
-    mutate(
-      est_lab_tree =
-        rpert(n = MC_n, mode = mean(set_lab), min = min(set_lab), max = max(set_lab)) +
-        runif(n = MC_n, min = min(prot_lab), max = max(prot_lab)) +
-        runif(n = MC_n, min = min(indimulch_lab), max = max(indimulch_lab)),
-      est_lab_m2 =
-        rep(contmulchon_lab, n = MC_n) +
-        rep(contmulchoff_lab, n = MC_n)
-    )
-  
-  # annual maintenance costs in ?
-  herb_cost <- c(0.15, 0.5) # herbicide cost per tree
-  
-  Dat_cost <- Dat_cost %>%
-    mutate(
-      maint_cost_tree =
-        runif(n = MC_n, min = min(herb_cost), max = max(herb_cost)),
-      maint_cost_m2 =
-        rep(0, n = MC_n)
-    )
-  
-  # annual maintenance labour in hours
-  herb_lab <- c(0.5, 0.2, 0.085, 0.06) / 60 # herbicide labour per m2
-  treemaint_lab <- c(1.15, 2.3, 1.1, 4) / 60 # annual maintenance labour, per tree
-  
-  Dat_cost <- Dat_cost %>%
-    mutate(
-      maint_lab_tree =
-        rpert(n = MC_n, mode = mean(treemaint_lab), min = min(treemaint_lab), max = max(treemaint_lab)),
-      maint_lab_m2 =
-        rpert(n = MC_n, mode = mean(herb_lab), min = min(herb_lab), max = max(herb_lab))
-    )
-  
-  # convert and select down
-  Dat_cost <- Dat_cost %>%
-    mutate(lab_rate = rpert(n = MC_n, min = 12, mode = 12.8, max = 14.05), # fmh 19/20 skilled labour hourly rate
-           est_cost_tree = est_cost_tree + est_lab_tree * lab_rate,
-           est_cost_m2 = est_cost_m2 + est_lab_m2 * lab_rate,
-           maint_cost_tree = maint_cost_tree + maint_lab_tree * lab_rate,
-           maint_cost_m2 = maint_cost_m2 + maint_lab_m2 * lab_rate) %>%
-    select(-est_lab_tree, -est_lab_m2, -maint_lab_tree, -maint_lab_m2, -lab_rate) %>%
-    mutate(cost_tree =
-             maint_cost_tree +
-             annualise_cost(est_cost_tree,
-                            lifespan = felling_age,
-                            dr = discount_rate),
-           cost_m2 =
-             maint_cost_m2 +
-             annualise_cost(est_cost_m2,
-                            lifespan = felling_age,
-                            dr = discount_rate)) %>%
-    select(-maint_cost_tree, -est_cost_tree, -maint_cost_m2, -est_cost_m2)
-  
-  cost_tree <- Dat_cost %>% pull(cost_tree) %>% mean()
-  cost_ha <- (Dat_cost %>% pull(cost_m2) %>% mean()) * 10^4
-  
-  # add fmh estimate for fencing
+  # fencing costs
   belt_circ <- length * 2 + width * 2
   beltarea_ha <- length * width * 10^-4
   fencelength_ha_m <- belt_circ / beltarea_ha
   
-  cost_ha <- cost_ha + fencelength_ha_m * 10
+  # fmh 19/20, £5.57 per m for deer fence
+  # much labour accounted for already in tree planting etc. so assume labour cost absorbed
+  # est 30 year lifespan for fence https://www.deerbusters.com/blog/poly-deer-fences-vs-steel-deer-fences/
+  deerfence_gbp_m <- 5.57
+  regfence_gbp_m <- 3.12
+  deerfencecost_ha_gbp <- annualise_cost(fencelength_ha_m * deerfence_gbp_m, felling_age, dr = discount_rate)
+  regfencecost_ha_gbp <- annualise_cost(fencelength_ha_m * regfence_gbp_m, felling_age, dr = discount_rate)
+  
+  # cost dataset
+  Dat_cost <- read_rds("simulation-base-data/crop-base-data.rds") %>%
+    distinct(crop) %>%
+    
+    # establishment costs from fmh 19/20 (pg 319)
+    # maintenance costs from Trees For Shelter (1997) -- FC x SAC tech note
+    mutate(set_cost_tree = annualise_cost(3250 / 7220, felling_age, discount_rate),
+           plant_cost_tree = annualise_cost(1080 / 7220, felling_age, discount_rate),
+           fence_cost_ha = ifelse(crop == "upland" | crop == "pasture", deerfencecost_ha_gbp, regfencecost_ha_gbp),
+           maint_cost_ha = c(30, 20, 30, 30, 30, 30, 30, 30, 30, 30) * 1.832) %>% # inf fac https://www.in2013dollars.com/uk/inflation/1997
+    
+    mutate(cost_tree = set_cost_tree + plant_cost_tree,
+           cost_ha = fence_cost_ha + maint_cost_ha) %>%
+    select(crop, cost_tree, cost_ha)
   
   # timber (rough wood) revenue
   rev_tonne <- mean(c(7, 30)) # rough logs value, gbp / tonne, fmh 19/20
-  timbrev <- tibble(wood_type = c("H", "S"),
-                    density = c(1.25, 1.43)  # m3 / tonne, https://www.forestresearch.gov.uk/tools-and-resources/statistics/forestry-statistics/forestry-statistics-2016-introduction/sources/timber/conversion-factors/
-                    ) %>%
+  Dat_rev <- tibble(wood_type = c("H", "S"),
+                    density = c(1.25, 1.43)) %>%  # m3 / tonne, https://www.forestresearch.gov.uk/tools-and-resources/statistics/forestry-statistics/forestry-statistics-2016-introduction/sources/timber/conversion-factors/
     mutate(rev_m3 = rev_tonne / density) %>%
     select(-density)
   
   df <- df %>%
-    left_join(timbrev, by = "wood_type") %>%
+    left_join(Dat_cost, by = "crop") %>%
+    left_join(Dat_rev, by = "wood_type") %>%
     mutate(ntrees = planted_area_ha * (100 / spacing_m)^2,
            timbrev_gbp = annualise_return(rev_m3 * treevol_m3ha * planted_area_ha, felling_age, discount_rate),
            timbcost_gbp = cost_tree * ntrees + cost_ha * planted_area_ha,
            timbgm_gbp = timbrev_gbp - timbcost_gbp) %>%
     select(-rev_m3)
-  
-  return(df)
-  
 }
-
-Dat_dummy <- Dat_dummy %>%
-  add_agf_margins(felling_age = 60,
-                  length = 180,
-                  width = 10,
-                  discount_rate = 0.035)
 
 #####################################
 # final estimates of carbon sequestration
 #####################################
-
-add_abatement <- function(df, felling_age){
+sb_env$add_abatement <- function(df, felling_age){
   
   # add ipcc soc scs calculations
   df <- df %>%
@@ -250,7 +159,37 @@ add_abatement <- function(df, felling_age){
   return(df)
 }
 
-Dat_dummy <- Dat_dummy %>%
-  add_abatement(felling_age = 60)
-
-# final mac / abatement + wrapper
+#####################################
+# wrapper function
+#####################################
+build_sb_agf <- function(spp_short,
+                         felling_age,
+                         discount_rate,
+                         belt_spacing = 250,
+                         length = 180,
+                         width = 10,
+                         applies_to = c("barley",
+                                        "cereals_other",
+                                        "oil_crops_other",
+                                        "pasture",
+                                        #"potato",
+                                        "pulses_other",
+                                        "rapeseed",
+                                        "upland",
+                                        #"vegetable",
+                                        "wheat")
+) {
+  
+  source("scenario-functions/univ-functions.R")
+  
+  read_rds("simulation-base-data/crop-base-data.rds") %>%
+    filter_crops(applies_to) %>%
+    sb_env$add_tree_data(spp_short, felling_age) %>%
+    sb_env$scale_system(belt_spacing, width, length) %>%
+    sb_env$add_crop_impacts() %>%
+    sb_env$add_agf_margins(felling_age, length, width, discount_rate) %>%
+    sb_env$add_abatement(felling_age) %>%
+    calc_mac() %>%
+    return()
+  
+}
--- scenario-runs.R
+++ scenario-runs.R
@@ -5,14 +5,11 @@ library(mc2d)
 # load base data
 Dat_crop <- read_rds("simulation-base-data/crop-base-data.rds")
 
-# source universal functions
-source("scenario-functions/univ-and-plot-functions.R")
-
 # dummy data, adjusted to 1 ha area per row to allow sensechecking aggregate calcs
 # use as needed
 set.seed(2605)
 Dat_dummy <- Dat_crop %>%
-  filter_crops(applies_to = "upland") %>%
+  #filter_crops(applies_to = "upland") %>%
   sample_n(25, replace = F) %>%
   mutate(croprev_gbp = croprev_gbp / area_ha,
          varcosts_gbp = varcosts_gbp / area_ha,
@@ -23,37 +20,79 @@ Dat_dummy <- Dat_crop %>%
 
 # row agroforestry
 source("scenario-functions/row-agf-functions.R")
-Dat_scen1 <- build_row_agf(felling_age = 60, row_spacing = 30, discount_rate = 0.035)
+Dat_row1 <- build_row_agf(felling_age = 60, row_spacing = 30, discount_rate = 0.035)
+Dat_row1 %>% pull(mac_gbp_tco2) %>% qplot()
 
-Dat_scen1 %>% pull(mac_gbp_tco2) %>% qplot()
+# shelter belt agroforestry
+source("scenario-functions/shelter-belt-functions.R")
+read_rds("shelter-belt-data-preprocessing/shelter-belt-data-clean.rds") %>%
+  distinct(spp, full_name)
+
+Dat_sb1 <- build_sb_agf(spp_short = "SAB", felling_age = 60, discount_rate = 0.035)
+Dat_sb1 %>% pull(mac_gbp_tco2) %>% qplot()
+
+Dat_sb2 <- build_sb_agf(spp_short = "SP", felling_age = 40, discount_rate = 0.035)
+Dat_sb2 %>% pull(mac_gbp_tco2) %>% qplot()
+
+##########################
+# plots
+##########################
+source("scenario-functions/post-and-plot-functions.R")
 
 ##########################
 # UK full MACCs
 ##########################
 
-# arable + pasture
-Dat_scen1 %>%
+# row, arable + pasture
+Dat_row1 %>%
   build_macc_plot() +
   labs(title = "UK MAC curve for row agroforestry",
        subtitle = "30m spacing\n60 year lifespan\n3.5% DR")
 
-ggsave("output-plots/uk-full-macc.png", width = 8, height = 5)
+ggsave("output-plots/uk-full-macc-row-agf-30-m-60-y-035-dr.png", width = 8, height = 5)
 
-# arable only
-Dat_scen1 %>%
+# row, arable only
+Dat_row1 %>%
   filter(crop != "pasture") %>%
   build_macc_plot()
 
-# pasture only
-Dat_scen1 %>%
+# row, pasture only
+Dat_row1 %>%
   filter(crop == "pasture") %>%
   build_macc_plot()
 
-# MAC map
-Dat_scen1 %>%
+# sb1, arable, pasture and upland
+Dat_sb1 %>%
+  build_macc_plot() +
+  labs(title = "UK MAC curve for shelter belt agroforestry",
+       subtitle = "Sycamore/ash/birch spp\n60 year lifespan\nBelt dimensions 180m x 10m\nBelt spacing 250m\n3.5% DR")
+
+ggsave("output-plots/uk-full-macc-shelt-belt-sab-60-y-180-10-250-m-035-dr.png", width = 8, height = 5)
+
+Dat_sb2 %>%
+  build_macc_plot() +
+  labs(title = "UK MAC curve for shelter belt agroforestry",
+       subtitle = "Scots pine spp\n40 year lifespan\nBelt dimensions 180m x 10m\nBelt spacing 250m\n3.5% DR")
+
+ggsave("output-plots/uk-full-macc-shelt-belt-sp-40-y-180-10-250-m-035-dr.png", width = 8, height = 5)
+
+##########################
+# UK MACC maps
+##########################
+
+# row, MAC map
+Dat_row1 %>%
   build_macc_map() +
   labs(title = "UK MAC map for row agroforestry",
        subtitle = "30m spacing\n60 year lifespan\n3.5% DR")
 
-ggsave("output-plots/uk-macc-map.png", width = 8, height = 8)
+ggsave("output-plots/uk-mac-map-row-agf-30-m-60-y-035-dr.png", width = 8, height = 8)
+
+Dat_sb1 %>%
+  build_macc_map() +
+  labs(title = "UK MAC map for shelter belt agroforestry",
+       subtitle = "SAB spp\n60 year lifespan\nBelt dimensions 180m x 10m\nBelt spacing 250m\n3.5% DR")
+
+ggsave("output-plots/uk-mac-map-shelt-belt-sab-60-y-180-10-250-m-035-dr.png", width = 8, height = 8)
+
   
